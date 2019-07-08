f07b3dd5331a5b1d56dbba1a1ae05ac8c7a824d3
Mike <michael.barbone@gmail.com>
2019-07-08 11:32:39 -0400

    Create callbacks for each api call and exoprt into separate files to make project more modular

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
 require("dotenv").config();
-const request = require("request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const weatherUrl = `https://api.darksky.net/forecast/${
-  process.env.WEATHER_TOKEN
-}/37.8267,-122.4233`;
-
-request({ url: weatherUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to weather service.");
-  } else if (response.body.error) {
-    console.log("Unable to find location.");
-  } else {
-    console.log(
-      `It is currently ${
-        response.body.currently.temperature
-      } degrees. There is  ${
-        response.body.currently.precipProbability
-      }% chance of rain.`
-    );
-  }
+geocode("New York City", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
 
-const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/clifton.json?access_token=${
-  process.env.GEOCODE_TOKEN
-}&limit=1`;
-
-request({ url: geoUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to geocoding service.");
-  } else if (response.body.message) {
-    console.log("No Location provided.");
-  } else if (response.body.features.length === 0) {
-    console.log("Location not found");
-  } else {
-    console.log(
-      response.body.features[0].center[0] +
-        ", " +
-        response.body.features[0].center[1]
-    );
-  }
+forecast(37.8267, -122.4233, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
---
+++ utils/forecast.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/${
+    process.env.WEATHER_TOKEN
+  }/${latitude},${longitude}`;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service.");
+    } else if (response.body.error) {
+      callback("Unable to find location.");
+    } else {
+      callback(
+        undefined,
+        `It is currently ${
+          response.body.currently.temperature
+        } degrees. There is  ${
+          response.body.currently.precipProbability
+        }% chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
